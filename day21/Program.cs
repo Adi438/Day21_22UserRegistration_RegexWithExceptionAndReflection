d7b7b7b8e20ca42fbdb429dccc7e95e0cda1e7b7
Adi438 <mishraaaditya54@gmail.com>
2022-06-02 00:52:55 +0530

    adding code

--- day21/Program.cs
+++ day21/Program.cs
@@ -1,15 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using UserRegistrationWithExceptionAndReflaction;
 
-namespace day21
+namespace MsTestUserRegistration
 {
-    internal class Program
+    [TestClass]
+    public class UserTest
     {
-        static void Main(string[] args)
+        Validation validation;
+        [TestInitialize]
+        public void SetUp()
         {
+            // common Arrange
+            validation = new Validation();
+        }
+        [TestMethod]
+        //Checking for multiple first name
+        [DataRow("Praful", "Input is valid")]
+        [DataRow("pr", "Input is not valid")]
+        [DataRow("praful", "Input is not valid")]
+        [DataRow("", "Input should not be empty")]
+        [DataRow(null, "Input should not be null")]
+        public void GivenFirstNameValidation(string firstName, string expected) // Testing for Firstname Validation
+        {
+            try
+            {
+                //Act
+                string actual = validation.FirstNameValidation(firstName);
+                //Assert
+                Assert.AreEqual(expected, actual);
+            }
+            catch (InvalidException ex) // catch exception if input is not valid or null or empty
+            {
+                Assert.AreEqual(expected, ex.Message);
+            }
         }
     }
 }
